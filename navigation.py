e07f5290805f5f058d44e960f349318326ee1637
Rukaiya Khondaker <rnk23@ic.ac.uk>
2026-02-11 14:40:46 +0000

    debugging 2

--- navigation.py
+++ navigation.py
@@ -20,7 +20,7 @@ def navigate_to_waypoint(waypoint, particles, weights):
     phi = np.rad2deg(np.arctan((w_y - robot_y) / (w_x - robot_x)))
     print("turn: ", -phi-robot_theta)
 
-    #particles = turnAntiClockwise(particles, phi+robot_theta)
+    particles = turnAntiClockwise(particles, phi+robot_theta)
     #particles = forward(particles, distance)
 
     return particles
